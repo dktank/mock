7e65689f15c4c017bf892b52a49e36de4edf4231
w1206408791 <1206408791@qq.com>
2018-08-01 12:32:55 +0800

    add get request and get port test

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,13 +1,24 @@
 import tornado.web
-
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hello,get")
-        #self.render('input.html')
+        data = {"1":{  "args": {},
+                  "headers": {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                    "Accept-Encoding": "gzip, deflate",
+                    "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6",
+                    "Connection": "close",
+                    "Cookie": "_gauges_unique_day=1; _gauges_unique_month=1; _gauges_unique_year=1; _gauges_unique=1",
+                    "Host": "httpbin.org",
+                    "Upgrade-Insecure-Requests": "1",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+                  },
+                  "origin": "124.65.37.238",
+                  "url": "http://httpbin.org/get"
+                   }
+          }
+        self.write(data)
 
 class ContentHandler(tornado.web.RequestHandler):
     def post(self):
         self.write("hello,post")
-        # inpcont = self.get_argument('inpcont')
-        # inpcont1 = self.get_argument('inpcont1')
-        # self.render('content.html', content={"name":inpcont,"password":inpcont1})
+
--- test/test.py
+++ test/test.py
@@ -1,23 +1,28 @@
 import unittest
-from tornado.testing import AsyncHTTPTestCase
-from App import application
+import requests
 
-class testhttp(AsyncHTTPTestCase):
-    def get_app(self):
-        return application.application
+class testhttp(unittest.TestCase):
+    def setUp(self):
+        self.get_url = "http://127.0.0.1:8008/get"
 
     #测试get请求
     def test_get(self):
-        response = self.fetch('/get')
-        self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, b'hello,get')
+        '''测试get请求的json数据'''
+        resp = requests.get(self.get_url)
+        data = resp.json()["1"]
+        self.assertEqual(data["args"],{})
+        self.assertEqual(data["headers"]["Accept"],"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+        self.assertEqual(data["headers"]["Accept-Encoding"],"gzip, deflate")
+        self.assertEqual(data["headers"]["Accept-Language"],"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6")
+        self.assertEqual(data["headers"]["Connection"],"close")
+        self.assertEqual(data["headers"]["Cookie"],"_gauges_unique_day=1; _gauges_unique_month=1; _gauges_unique_year=1; _gauges_unique=1")
+        self.assertEqual(data["headers"]["Host"],"httpbin.org")
+        self.assertEqual(data["headers"]["Upgrade-Insecure-Requests"],"1")
+        self.assertEqual(data["headers"]["User-Agent"],"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+        self.assertEqual(data["origin"],"124.65.37.238")
+        self.assertEqual(data["url"],"http://httpbin.org/get")
 
-    #测试post请求
-    def test_post(self):
-        body = ''
-        response = self.fetch('/post',method='POST',body=body)
-        self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, b'hello,post')
+
 
 if __name__ == "__main__":
     unittest.main()
