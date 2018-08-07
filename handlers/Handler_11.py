3542408f5048506d9461bf1235638d52035d6c3b
w1206408791 <1206408791@qq.com>
2018-08-07 11:56:42 +0800

    add 2018/8/7

---
+++ handlers/Handler_11.py
@@ -0,0 +1,17 @@
+from config.PublicClass import Request
+
+#|http://httpbin.org/get|get|11|JSON字符串|No.149|
+#带参数{'name':'张三', 'hobbies': ['篮球','足球','羽毛球'], 'transcripts':{'语文':30, '数学': 99.5}}
+
+class  GetHandler(Request):
+    def get(self):
+        try:
+            Request.result1(self)
+            profile = self.get_argument("profile")#获取参数profile的值
+            args = {'profile':profile}
+            self.data["args"] = args
+            self.data["headers"]["Host"] = self.request.host
+            print(self.data)
+            self.write(self.data)
+        except:
+            self.send_error(status_code=404)
--- handlers/url.py
+++ handlers/url.py
@@ -1,12 +1,12 @@
-from handlers import IndexHandler,Handler136,Handler137,Handler138,Handler139,Handler141,Handler142,Handler145,Handler149
+from handlers import IndexHandler,Handler_1,Handler_2,Handler_3,Handler_4,Handler_139,Handler_5,Handler_6,Handler_10,Handler_11
 
 urls = [
        (r'/',IndexHandler.IndexHandler),
-       (r'/get', Handler137.GetHandler),
-       (r'/post', Handler137.PostHandler),
-       (r'/headers', Handler138.GetHandler),
-       (r'/status/201',Handler139.Status201Handler),
-       (r'/status/400',Handler139.Status400Handler),
-       (r'/status/500',Handler139.Status500Handler),
-       (r'/cookies/set',Handler145.SetCookiesHandler)
+       (r'/get', Handler_11.GetHandler),
+       (r'/post', Handler_10.PostHandler),
+       (r'/headers', Handler_4.GetHandler),
+       (r'/status/201',Handler_139.Status201Handler),
+       (r'/status/400',Handler_139.Status400Handler),
+       (r'/status/500',Handler_139.Status500Handler),
+       (r'/cookies/set',Handler_10.SetCookiesHandler)
       ]
