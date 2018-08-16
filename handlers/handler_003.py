f12750cdc699c1590a1a743e7335527cb57c3c58
w1206408791 <1206408791@qq.com>
2018-08-16 12:06:45 +0800

    Rewrite the 1235 protocol

--- handlers/handler_003.py
+++ handlers/handler_003.py
@@ -2,13 +2,13 @@ from handlers.public import Request
 
 
 class PostHandler(Request):
-    """该Handler完成编号3协议：|/post|post|3|JSON字符串|No.137<br>带参数k1=v1&k2=v2|"""
+    """该Handler完成编号3，5协议：/httpbin/post|post|3|JSON|No.137<br> body{ "k1": "v1", "k2": "v2"} |
+                             和/httpbin/post|post|5|JSON|No.141<br>带参数<br>{'k2': 'v2', 'k1': ['v1', 'v3']}|
+    """
 
     def post(self):
         """处理POST请求
 
-        变量k1、k2存放请求中的k1、k2参数且这两个参数都是包含单个值，
-        变量form是包含着k1、k2变量的字典用于赋值给self.data["form"]
         """
         data = {
               "args": {},
